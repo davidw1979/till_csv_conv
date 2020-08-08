21275bfb8985e7309fd69e23889b51acefc15eb3
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-08-08 16:30:33 +0100

    First working version with instruction in README. Manipulates input epos programming csv ready for upload as per tilltech specification.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # till_csv_conv
-Python script using pandas to manipulate epos CSV menu files for re-upload
+Python script using pandas to manipulate epos CSV menu files for re-upload.
+
+To use you will require numpy & pandas libraries as dependancies.
+Script needs to be located in a folder with the original csv file requiring conversion
+program is to be ran from the command terminal.
+It takes one additional argument - the csv file to be converted.
+
+Example:
+python3 till_csv_conv.py test.csv
+
+The output file (output.csv) is automatically saved in the same folder. 
---
+++ epos_csv_generator.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import sys
+
+
+def add_blank_rows(qty, df):
+    # create blank row
+    blank_row = pd.DataFrame([[np.nan] * len(df.columns)], columns=df.columns)
+
+    # add blank rows
+    for i in range(0, qty):
+        # append single blank row to dataframe
+        df = df.append(blank_row)
+
+    # reset index
+    df = df.reset_index(drop=True)
+
+    # correct record values
+    for index, row in df.iterrows():
+        if row["Record"] != index + 1:
+            df.loc[index, "Record"] = index + 1
+
+    return df
+
+
+def calc_blanks(num):
+    if num < 50:
+        req = 50 - num
+    else:
+        req = 50 - (num % 50)
+
+    return req
+
+
+def main():
+    # check argument usage is correct
+    if len(sys.argv) != 2:
+        exit("Incorrect usage. This program takes 1 command line argument - the csv filename.")
+
+    # load files & set start & highest department variables
+    df = pd.read_csv(sys.argv[1])
+    current_dept = 1
+    final_dept = df.iloc[len(df) - 1]["Department Link"]
+
+    # set new dataframe as data with first Department Link
+    new_df = df.drop(df[df['Department Link'] != current_dept].index)
+
+    # generate required blank rows for new dataframe
+    blanks_needed = calc_blanks(len(new_df))
+    new_df = add_blank_rows(blanks_needed, new_df)
+
+    # main loop -> iterate over all department link groups
+    while current_dept < final_dept:
+        # create temp dataframe of next department link value
+        current_dept += 1
+        temp_df = df.drop(df[df['Department Link'] != current_dept].index)
+
+        # calculate blank rows to take upto next 50 for temp
+        blanks_needed = calc_blanks(len(temp_df))
+
+        # join onto final dataframe and add required blank rows
+        new_df = new_df.append(temp_df)
+        new_df = add_blank_rows(blanks_needed, new_df)
+
+    # create output csv file
+    new_df.to_csv('output.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
